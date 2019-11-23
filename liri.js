9acdd8de1da2d0932f8bc44a0e385970a39effb3
JaneBane <lienaqperez19@gmail.com>
2019-11-23 18:01:37 -0500

    changes to js file

--- liri.js
+++ liri.js
@@ -1,111 +1,189 @@
+// Info for Spotify
 require("dotenv").config();
-
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
-var Spotify = require('node-spotify-api'); //Using the Spotify api and getting the key from keys.js
 var spotify = new Spotify(keys.spotify);
 
-var moment = require('moment'); //Both required to use moment for node
-moment().format();
+// Info for Request
+var request = require("request");
 
-var axios = require('axios'); //To get the information from the APIs for movie and concert-this
+// Info for Moment -- Date Formatting
+var moment = require("moment");
 
-var fs = require('fs'); //To read the random.txt file for the do-what-it-says function
+// Info for fs
+var fs = require("fs");
 
-var command = process.argv[2]; //For the switch statement
-var value = process.argv[3]; //To send the song/movie/concert to their functions
 
-switch (command) {
-    case "concert-this":
-        concertThis(value);
-        break;
-    case "spotify-this-song":
-        spotifySong(value);
-        break;
-    case "movie-this":
-        movieThis(value);
-        break;
-    case "do-what-it-says":
-        doThis(value);
-        break;
-};
+// Command
+var command = process.argv[2];
 
-function concertThis(value) {
-    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
-    .then(function(response) {    
-        for (var i = 0; i < response.data.length; i++) {
 
-            var datetime = response.data[i].datetime; //Saves date time response into a variable
-            var dateArr = datetime.split('T'); 
+// CONCERT
+if (command === "concert-this") {
 
-            var concertResults = 
-                "--------------------------------------------------------------------" +
-                    "\nVenue Name: " + response.data[i].venue.name + 
-                    "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
-            console.log(concertResults);
+    var artist = process.argv[3];
+
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("-------------------------------------");
+            console.log("Venue: " + JSON.parse(body)[0].venue.name);
+            console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
+            console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+            console.log("-------------------------------------");
         }
-    })
-    .catch(function (error) {
-        console.log(error);
     });
-        
 
-}
 
-function spotifySong(value) {
-    if(!value){
-        value = "The Sign";
+    // SONG
+} else if (command === "spotify-this-song") {
+    var song = process.argv[3];
+
+    if (song === undefined) {
+        song = "The Sign";
     }
-    spotify
-    .search({ type: 'track', query: value })
-    .then(function(response) {
-        for (var i = 0; i < 5; i++) {
-            var spotifyResults = 
-                "--------------------------------------------------------------------" +
-                    "\nArtist(s): " + response.tracks.items[i].artists[0].name + 
-                    "\nSong Name: " + response.tracks.items[i].name +
-                    "\nAlbum Name: " + response.tracks.items[i].album.name +
-                    "\nPreview Link: " + response.tracks.items[i].preview_url;
-                    
-            console.log(spotifyResults);
+
+    spotify.search({
+        type: "track",
+        query: song
+    }, function (err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
         }
-    })
-    .catch(function(err) {
-        console.log(err);
+        console.log("-------------------------------------");
+        console.log("Artist: " + data.tracks.items[0].artists.name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("-------------------------------------");
     });
-}
 
-function movieThis(value) {
-    if(!value){
-        value = "mr nobody";
+
+    // IF you want to know about a MOVIE
+} else if (command === "movie-this") {
+
+    var movie = process.argv[3];
+
+    if (movie === undefined) {
+        movie = "Mr. Nobody";
     }
-    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
-    .then(function(response) {
-            var movieResults = 
-                "--------------------------------------------------------------------" +
-                    "\nMovie Title: " + response.data.Title + 
-                    "\nYear of Release: " + response.data.Year +
-                    "\nIMDB Rating: " + response.data.imdbRating +
-                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
-                    "\nCountry Produced: " + response.data.Country +
-                    "\nLanguage: " + response.data.Language +
-                    "\nPlot: " + response.data.Plot +
-                    "\nActors/Actresses: " + response.data.Actors;
-            console.log(movieResults);
-    })
-    .catch(function (error) {
-        console.log(error);
+
+    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            // Information about Movie
+            console.log("-------------------------------------");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year Released: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country Produced: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("-------------------------------------");
+        }
     });
-    
-}
 
-function doThis(value) {
+    // WILDCARD
+} else if (command === "do-what-it-says") {
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
         if (error) {
             return console.log(error);
         }
-        var dataArr = data.split(',');
-        spotifySong(dataArr[0], dataArr[1]);
-    })
+
+        console.log(data);
+        var dataArr = data.split(",");
+        console.log(dataArr);
+
+        command = dataArr[0];
+        whatToCommand = dataArr[1];
+
+        if (command === "concert-this") {
+
+            var artist = whatToCommand;
+
+            request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    console.log("-------------------------------------");
+                    console.log("Venue: " + JSON.parse(body)[0].venue.name);
+                    console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
+                    console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+                    console.log("-------------------------------------");
+                }
+            });
+
+
+            // IF WILDCARD want to know about a SONG
+        } else if (command === "spotify-this-song") {
+            var song = whatToCommand;
+
+            if (song === undefined) {
+                song = "The Sign";
+            }
+
+            spotify.search({
+                type: "track",
+                query: song
+            }, function (err, data) {
+                if (err) {
+                    return console.log("Error occured: " + err);
+                }
+                console.log(data.tracks.items[0].album[0]);
+                // console.log("-------------------------------------");
+                // console.log("Command: ", command, ", Song Name: ", song);
+                // console.log("Artist: " + data);
+                // console.log("Song Name: " + data);
+                // console.log("Preview Link: " + data);
+                // console.log("Album: " + data);
+                // console.log("-------------------------------------");
+            });
+
+
+            // IF WILDCARD wants to know about a MOVIE
+        } else if (command === "movie-this") {
+
+            var movie = whatToCommand;
+
+            if (movie === undefined) {
+                movie = "Mr. Nobody";
+            }
+
+            request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+
+                    // Information about Movie
+                    console.log("-------------------------------------");
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Year Released: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country Produced: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                    console.log("-------------------------------------");
+                }
+            });
+
+
+            // IF Command not entered or incorrectly entered
+        } else {
+            console.log("Command Error");
+        }
+
+        console.log("-------------------------------------");
+        console.log("Command: ", command);
+        console.log("-------------------------------------");
+    });
+
+
+    // IF Command not entered or incorrectly entered
+} else {
+    console.log("Command Error");
 }
